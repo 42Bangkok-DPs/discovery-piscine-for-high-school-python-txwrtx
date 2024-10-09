aaa9f378b213dc22b40622e944606c2d09f5c320
txwrtx <110953748+txwrtx@users.noreply.github.com>
2024-10-09 12:22:11 +0700

    Delete cell03/ex0 directory

--- cell03/ex0/i_got_that.py
+++
@@ -1,7 +0,0 @@
-text = "-1"
-
-while True :
-    if text == "-1": text = str(input("What you gotta say? : "))
-    else : text = str(input("I got that! Anything else? : "))
-
-    if text == "STOP" :break;
