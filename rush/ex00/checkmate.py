f550ef8be806e0d000feb2878c73dcbc88675bf3
txwrtx <110953748+txwrtx@users.noreply.github.com>
2024-10-11 13:08:21 +0700

    Update checkmate.py

--- rush/ex00/checkmate.py
+++ rush/ex00/checkmate.py
@@ -1,3 +1,8 @@
+"""
+    Add this chat_list for error handling
+    char_list = [ 'R', 'B', 'Q', 'P', 'K', '.' ]
+"""
+
 directions = {
     'R': [(0, 1), (0, -1), (1, 0), (-1, 0)],
     'B': [(1, 1), (1, -1), (-1, 1), (-1, -1)],
@@ -15,6 +20,31 @@ def checkmate(board=""):
         
     board = create_board(board)
     king_pos = find_king_pos(board)
+
+    """
+        Add this line of code to handle error if 
+        there's king more than 1 king and if there's 
+        Char that's not in the game just return error
+        
+        found_king = 0;
+        for row in range(len(board)):
+            for col in range(len(board)):
+                if board[row][col] == "K": found_king += 1
+    
+                if board[row][col] not in char_list:
+                    return print("Error")
+        if found_king > 1 :
+            return print("Error")
+    """
+    
+
+    """
+        Add this lines of code to check if 
+        king is equal to None then return Error
+    
+        if king_pos == None:
+        return print("Error")
+    """
 
     for piece, moves in directions.items():
         for move in moves:
@@ -39,6 +69,7 @@ def checkmate(board=""):
 def create_board(board):
     return [list(row) for row in board]
 
+# Change to return None to handle if there's no king on the tale
 def find_king_pos(board):
     for r in range(len(board)):
         for c in range(len(board)):
